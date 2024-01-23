05c70f6e251efa91febed126809b58c64f335acb
ellen-ls <ellen.l.s1996@gmail.com>
2024-01-23 19:19:15 -0300

    projeto realizado com sucesso

---
+++ dump.sqlite.sql
@@ -0,0 +1,18 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE 'fixed_database_1' (data TEXT,id_marca_ INTEGER,vendas INTEGER,valor_do_veiculo INTEGER,nome TEXT);
+CREATE TABLE 'fixed_database_2' (id_marca INTEGER,marca TEXT);
+CREATE TABLE Vendas (
+  data DATE,
+  id_marca INTEGER PRIMARY KEY,
+  vendas REAL,
+  valor_do_veiculo REAL,
+  nome TEXT
+, marca TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
